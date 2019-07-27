62bce041f8f532a324c049e9f297b74e9013e71b
Jayson Chelliah <jcraul7@yahoo.com>
2019-07-28 00:17:03 +0800

    Installed Redux and moved dotenv to index.css

--- src/App.js
+++ src/App.js
@@ -4,13 +4,51 @@ import './App.css';
 import Header from './components/Header'
 import Map from './components/Map';
 
-require('dotenv').config()
-
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      applicationName:'TrafficMap'
+      applicationName:'TrafficMap',
+      date:null,
+      api_url: 'https://data.edmonton.ca/resource/87ck-293k.json',
+    }
+  }
+
+  componentDidMount() {
+    const { data, api_url } = this.state;
+
+    if (!data) {
+        fetch(api_url, { method: 'GET' })
+            .then(response => response.json())
+            .then(response => this.createFeatureCollect(response))
+            .then(response => this.setState({ data: response }));
+    }
+  }
+
+  createFeatureCollect(data) {
+    let features = [];
+    data.forEach(point => {
+        features.push({
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    parseFloat(point.location.longitude),
+                    parseFloat(point.location.latitude)
+                ]
+            },
+            "properties": {
+                "description": point.description,
+                "details": point.details,
+                "duration": point.duration,
+                "impact": point.impact
+            }
+        });
+    });
+
+    return {
+        "type": "FeatureCollection",
+        "features": features
     }
   }
 
@@ -19,7 +57,7 @@ class App extends Component {
         <div className='App'>
           <Header appName={this.state.applicationName} />
           <Container>
-            <Map />
+            <Map data={this.state.data} />
           </Container>
         </div>
       );
